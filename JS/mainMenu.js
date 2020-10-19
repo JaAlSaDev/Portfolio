9b4422ab575f95ec10f7b754aeb3dfcfe7ca5ddc
Jaber Alsalamah <JaAlSaDev@gmail.com>
2020-10-19 13:33:28 +0300

    refactored job title variables and functions into JSON.

--- JS/mainMenu.js
+++ JS/mainMenu.js
@@ -4,64 +4,63 @@ window.$ = window.jQuery = jQuery;
 export let mainMenu = {
     elem: $("#MainMenu"),
     previewStatic: $("#previewStatic"),
-    jobTitleElm: $("#JobTitle"),
-    jobtitles: ['Software Engineer', 'Full Stack Developer', 'Game Developer', 'Creative Spirit'],
 
-    jobTitleIndex: 0,
-    writeDuration: 100,
-    deleteDuration: 50,
-    waitDuration: 2000,
+    jobTitle: {
+        elem: $("#JobTitle"),
+        list: ['Software Engineer', 'Full Stack Developer', 'Game Developer', 'Creative Spirit'],
+        index: 0,
+        durations: {
+            write: 100,
+            delete: 50,
+            wait: 2000
+        },
+        write: function() {
+            if (!this.list[this.index]) {
+                this.index = 0;
+            }
 
-    typeJobTitle: function() {
-        if (!this.jobtitles[this.jobTitleIndex]) {
-            this.jobTitleIndex = 0;
+            const currentTitle = this.list[this.index];
+
+            currentTitle.split();
+
+            let printedText = '';
+            let currentChar = 0;
+
+
+            let int1 = setInterval(() => {
+                if (currentTitle[currentChar]) {
+                    //Print text if there are characters left
+                    printedText += currentTitle[currentChar++];
+                    this.elem.text(printedText)
+
+                } else {
+                    //Go to the next job title
+                    this.index++;
+
+                    setTimeout(() => {
+                        this.delete(printedText);
+                    }, this.durations.wait);
+                    clearInterval(int1);
+                }
+            }, this.durations.write);
+        },
+        delete: function(str) {
+            let int = setInterval(() => {
+                if (str.length !== 0) {
+
+                    str = str.split('');
+                    str.pop();
+                    str = str.join('');
+
+                    this.elem.text(str)
+                } else {
+                    setTimeout(() => {
+                        this.write();
+                    }, this.durations.wait);
+                    clearInterval(int);
+                }
+            }, this.durations.delete);
         }
-
-        const currentTitle = this.jobtitles[this.jobTitleIndex];
-
-        currentTitle.split();
-
-        let printedText = '';
-        let currentChar = 0;
-
-
-        let int1 = setInterval(() => {
-            if (currentTitle[currentChar]) {
-                //Print text if there are characters left
-                printedText += currentTitle[currentChar++];
-                this.jobTitleElm.text(printedText)
-
-            } else {
-                //Go to the next job title
-                this.jobTitleIndex++;
-
-                setTimeout(() => {
-                    this.deleteMessage(printedText);
-                }, this.waitDuration);
-                clearInterval(int1);
-            }
-        }, this.writeDuration);
-
-    },
-
-    deleteMessage: function(str) {
-        let int = setInterval(() => {
-            if (str.length !== 0) {
-
-                str = str.split('');
-                str.pop();
-                str = str.join('');
-
-                this.jobTitleElm.text(str)
-            } else {
-
-
-                setTimeout(() => {
-                    this.typeJobTitle();
-                }, this.waitDuration);
-                clearInterval(int);
-            }
-        }, this.deleteDuration);
     },
 
     staticFlicker: (duration) => {
@@ -72,7 +71,7 @@ export let mainMenu = {
             this.elem.fadeIn(3000);
             setTimeout(() => {
                 this.staticFlicker(2500);
-                this.typeJobTitle();
+                this.jobTitle.write();
                 setInterval(() => {
                     this.staticFlicker(2500);
                 }, 2750);
