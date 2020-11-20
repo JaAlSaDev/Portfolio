14aeb09290e3072277b574b0ffcd478be01ed5d0
Jaber Alsalamah <46474223+JaAlSaDev@users.noreply.github.com>
2020-11-20 21:56:23 +0300

    Setup (#1)
    * initialized project, added Start screen.
    
    * added a flicker effect to the Start prompt.
    
    * created startPrompt JSON.
    
    * Added mouse interaction for the start screen
    
    * added placeholder for main menu, added transition from start screen to main menu.
    
    * added two preview boxes on the main menu, added static to preview image.
    
    * added a job title paragraph with typing effect on the main menu.
    
    * added duration variables and values for typing the job title.
    
    * moved main menu code from index.js to mainMenu.js
    
    * refactored job title variables and functions into JSON.
    
    * added hexagon for about me in main menu.
    
    * skewed the preview containers on the main menu.
    
    * added five svg hexagons to the main screen.
    
    * added a background image and midsection to the main menu.
    
    * added different images to the hexagons
    
    * changed the body's width and height to percentages.
    
    * enabled hover interaction on hexagons.
    
    * created previewPanels JSON in mainMenu.js
    
    * added event handlers to hexagons, changed preview panel content depending on hexagon.
    
    * took out jobTitle, previewPanels, and hexagons from the mainMenu object.
    
    * added original and hover images to the hexagons.
    
    * added hover sound effect to hexagons.
    
    * preloaded hexagon images.
    
    * created the Hexagon class, removed hexagon elements from index.html.
    
    * created the links screen.

---
+++ JS/mainMenu.js
@@ -0,0 +1,272 @@
+import jQuery from "./jQuery";
+window.$ = window.jQuery = jQuery;
+import sundayImg from "../Images/onthesunday-background-compress.jpg"
+import handCannotErase from "../Images/qg26Ki.jpg"
+import astronaut from "../Images/tumblr_ockrhsElIo1tmnl7lo1_500.png";
+import sunday from "../Images/Sunday.jpg"
+import astronaut1 from "../Images/Astronaut1.jpg"
+import tvStatic from "../Images/Static3.gif";
+import recordings from "../Images/Recordings.png"
+import gameControllerIcon from "../svg/game-controller.svg";
+import webDevelopmentIcon from "../svg/programming.svg";
+import linkIcon from "../svg/link.svg"
+import githubIcon from "../svg/github.svg"
+import twitterIcon from "../svg/twitter.svg"
+import linkedinIcon from "../svg/linkedin.svg"
+import gmailIcon from "../svg/gmail.svg"
+import Hexagon from "./Hexagon";
+
+
+
+let audioElement = $("audio")[0]
+audioElement.src = require("../Sound/X8StageSelect.mp3")
+
+const audioContext = new AudioContext();
+
+const track = audioContext.createMediaElementSource(audioElement);
+track.connect(audioContext.destination)
+
+
+
+let jobTitle = {
+    elem: $("#JobTitle"),
+    list: ['Software Engineer', 'Full Stack Developer', 'Game Developer'],
+    index: 0,
+    durations: {
+        write: 100,
+        delete: 50,
+        wait: 2000
+    },
+    write: function() {
+        if (!this.list[this.index]) {
+            this.index = 0;
+        }
+
+        const currentTitle = this.list[this.index];
+
+        currentTitle.split();
+
+        let printedText = '';
+        let currentChar = 0;
+
+
+        let int1 = setInterval(() => {
+            if (currentTitle[currentChar]) {
+                //Print text if there are characters left
+                printedText += currentTitle[currentChar++];
+                this.elem.text(printedText)
+
+            } else {
+                //Go to the next job title
+                this.index++;
+
+                setTimeout(() => {
+                    this.delete(printedText);
+                }, this.durations.wait);
+                clearInterval(int1);
+            }
+        }, this.durations.write);
+    },
+    delete: function(str) {
+        let int = setInterval(() => {
+            if (str.length !== 0) {
+
+                str = str.split('');
+                str.pop();
+                str = str.join('');
+
+                this.elem.text(str)
+            } else {
+                setTimeout(() => {
+                    this.write();
+                }, this.durations.wait);
+                clearInterval(int);
+            }
+        }, this.durations.delete);
+    }
+}
+let previewPanels = {
+    img: $("#previewImg"),
+    previewStatic: $("#previewStatic"),
+    text: $("#TextPreview")[0],
+
+    changeContent: function(imgSrc, text) {
+        this.img.attr("src", imgSrc);
+        this.text.textContent = text;
+    },
+    staticFlicker: function(duration) {
+        this.previewStatic.fadeTo(duration / 2, 0.1).fadeTo(duration / 2, 0.5);
+    },
+};
+
+//Inject hexagons to the DOM
+{
+    //Hexagon attributes
+    let patternID = ["topLeftHexPatt", "topRightHexPatt", "midHexPatt", "bottomLeftHexPatt", "bottomRightHexPatt", "gitHub", "twitter", "linkedIn", "email"];
+    let location = ["#upperSection", "#upperSection", "#ImgPreviewContainer", "#lowerSection", "#lowerSection", "#topSection", "#topSection", "#bottomSection", "#bottomSection"]
+    let originalImages = [
+        tvStatic,
+        sunday,
+        "http://stevenwilsonhq.com/sw/wp-content/uploads/2014/12/HCE-finalcover.jpg",
+        astronaut1,
+        tvStatic
+    ];
+    let icons = [
+        [],
+        [],
+        [],
+        [{
+                "x": "0.25",
+                "y": "0.25",
+                "xlink:href": gameControllerIcon,
+                "transform": "scale(0.5)"
+            },
+            {
+                "x": "0.85",
+                "y": "0.85",
+                "xlink:href": webDevelopmentIcon,
+                "transform": "scale(0.45)"
+            }
+        ],
+        [{
+            "x": "0.35",
+            "y": "0.35",
+            "xlink:href": linkIcon,
+            "transform": "scale(0.6)"
+        }],
+        [{
+            "x": "0.35",
+            "y": "0.35",
+            "xlink:href": githubIcon,
+            "transform": "scale(0.6)"
+        }],
+        [{
+            "x": "0.35",
+            "y": "0.35",
+            "xlink:href": twitterIcon,
+            "transform": "scale(0.6)"
+        }],
+        [{
+            "x": "0.35",
+            "y": "0.35",
+            "xlink:href": linkedinIcon,
+            "transform": "scale(0.6)"
+        }],
+        [{
+            "x": "0.35",
+            "y": "0.35",
+            "xlink:href": gmailIcon,
+            "transform": "scale(0.6)"
+        }]
+    ];
+
+
+    //Inject the hexagons into the DOM
+    for (let i = 0; i < 2; i++) {
+
+        $(location[i]).append((new Hexagon(patternID[i], originalImages[i], icons[i])).getElement())
+    }
+
+    $(location[2]).after((new Hexagon(patternID[2], originalImages[2], icons[2])).getElement())
+
+    for (let i = 3; i < location.length; i++) {
+        $(location[i]).append((new Hexagon(patternID[i], originalImages[i], icons[i])).getElement())
+    }
+}
+
+
+
+let hexagons = {
+    containers: $("svg"),
+    patterns: $(".hexPattern"),
+    elements: (Object.values($(".hexagon"))),
+    originalImages: [
+        tvStatic,
+        sunday,
+        "http://stevenwilsonhq.com/sw/wp-content/uploads/2014/12/HCE-finalcover.jpg",
+        astronaut1,
+        tvStatic
+    ],
+    hoverImages: [
+        "https://media.giphy.com/media/Ph0oIVQeuvh0k/giphy.gif",
+        "https://neuralrustsite.files.wordpress.com/2017/05/04-tr-789-e1501090105324.png?w=700",
+        "http://stevenwilsonhq.com/sw/wp-content/uploads/2015/02/cryingeyes-hce.jpg",
+        "https://cdn.bandmix.com/bandmix_us/media/468/468750/1266987-l.jpg",
+        "https://media.giphy.com/media/Ph0oIVQeuvh0k/giphy.gif"
+    ],
+    previewTexts: [
+        "???",
+        "On the Sunday of Life",
+        "Hand Cannot Erase",
+        "Projects",
+        "Links"
+    ],
+    previewImgs: [
+        "https://media.giphy.com/media/Ph0oIVQeuvh0k/giphy.gif",
+        sundayImg,
+        handCannotErase,
+        astronaut,
+        recordings
+    ],
+    screenIDs: ["EducationScreen", "ExperienceScreen", "AboutMeScreen", "ProjectsScreen", "LinksScreen"],
+
+    addEventListeners: function() {
+        this.elements.splice(5);
+
+        this.elements.forEach((hexagon, index) => {
+            console.log(hexagon);
+            hexagon.addEventListener("mouseover", () => {
+                previewPanels.changeContent(this.previewImgs[index], this.previewTexts[index])
+
+                this.changeHexImage(index, this.hoverImages[index]);
+                audioElement.play();
+            })
+
+            hexagon.addEventListener("mouseleave", () => {
+                previewPanels.changeContent("", "")
+                    // audioElement.pause()
+                this.changeHexImage(index, this.originalImages[index]);
+            })
+
+            hexagon.addEventListener("click", () => {
+                $("#MainMenu").fadeOut(3000);
+                setTimeout(() => {
+
+                    $(`#${this.screenIDs[index]}`).fadeIn(5500)
+                    $(`#${this.screenIDs[index]}`).css("display", "flex");
+
+                }, 3000);
+            })
+        });
+    },
+
+    changeHexImage: function(index, imgSrc) {
+        this.patterns[index].firstElementChild.attributes[2].nodeValue = imgSrc;
+    }
+}
+
+
+export let mainMenu = {
+    elem: $("#MainMenu"),
+    control: function() {
+        setTimeout(() => {
+            this.elem.fadeIn(3000);
+            hexagons.addEventListeners();
+            setTimeout(() => {
+                previewPanels.staticFlicker(2500);
+                jobTitle.write();
+                setInterval(() => {
+                    previewPanels.staticFlicker(2500);
+                }, 2750);
+            }, 3000);
+
+        }, 3000);
+
+
+    }
+};
+
+for (let index = 0; index < hexagons.hoverImages.length; index++) {
+    var img = new Image();
+    img.src = hexagons.hoverImages[index];
+}
---
+++ index.js
@@ -0,0 +1,106 @@
+import jQuery from "./JS/jQuery";
+window.$ = window.jQuery = jQuery;
+import {
+    mainMenu
+} from "./JS/mainMenu"
+
+let startScreen = $("#StartScreen");
+
+let startPrompt = {
+    element: $("#StartPrompt"),
+    isFast: false,
+
+
+    changeText: function(newText) {
+        if (!this.isFast) {
+            this.element.text(newText);
+        }
+    },
+    flickerOnce: function(duration) {
+        this.element.fadeIn(duration / 2).fadeOut(duration / 2);
+    },
+
+    timer: (duration) => setInterval(() => {
+        startPrompt.flickerOnce(duration)
+    }, duration),
+
+    speedUp: function(flick, fastFlickerDuration) {
+        if (!this.isFast) {
+            clearInterval(flick)
+            this.timer(fastFlickerDuration);
+            this.isFast = true;
+        }
+
+    },
+
+    transitionToMainScreen: function(flickerTimer, fastDuration) {
+
+        //Speed up the start prompt
+        startPrompt.speedUp(flickerTimer, fastDuration);
+
+        setTimeout(() => {
+            startScreen.fadeOut(3000);
+
+            mainMenu.control();
+        }, 1250);
+
+
+
+    },
+
+    control: function(initialFadeInDuration, normalDuration, fastDuration) {
+
+        setTimeout(() => {
+            this.flickerOnce(normalDuration);
+
+            let flickerTimer = this.timer(normalDuration);
+
+            //Keyboard
+            window.addEventListener("keypress", function(e) {
+                e = e || window.event;
+
+                if (e.key) {
+                    startPrompt.changeText("PRESS ENTER");
+                }
+
+                if (e.key == "Enter") {
+                    startPrompt.transitionToMainScreen(flickerTimer, fastDuration)
+                }
+            })
+
+            //Mouse
+            window.onmousemove = function(e) {
+                startPrompt.changeText("CLICK HERE");
+            }
+
+            this.element.on("mouseup", function(e) {
+                if (e.button == 0) {
+                    startPrompt.transitionToMainScreen(flickerTimer, fastDuration)
+                }
+            })
+
+
+            //Gamepad
+            window.addEventListener("gamepadconnected", function(e) {
+                var gamepad = e.gamepad;
+                console.log(
+                    "Gamepad connected at index %d: %s. %d buttons, %d axes.",
+                    gamepad.index,
+                    gamepad.id,
+                    gamepad.buttons.length,
+                    gamepad.axes.length
+                );
+
+                startPrompt.changeText("PRESS START")
+
+            });
+
+            window.addEventListener("gamepaddisconnected", function(e) {
+                startPrompt.changeText("PRESS ENTER");
+            });
+
+        }, initialFadeInDuration);
+    }
+}
+
+startPrompt.control(2500, 1250, 100);
