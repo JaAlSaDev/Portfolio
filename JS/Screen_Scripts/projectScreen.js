45498d42526910971954545ad70e2b6c51119196
Jaber Alsalamah <JaAlSaDev@gmail.com>
2021-05-10 15:35:45 +0300

    created the options object inside the overlayMenu object.

--- JS/Screen_Scripts/projectScreen.js
+++ JS/Screen_Scripts/projectScreen.js
@@ -6,72 +6,120 @@ import {
 import Hexagon from "../Hexagon/Hexagon";
 import stevenSketch from "../../assets/Images/Stupid_Dream.jpg";
 import soundEffects from "../Sound/soundEffects"
-import {SETTINGS} from "../../settings"
+import { SETTINGS } from "../../settings"
 
 
-//Inject the hexagons into the DOM
-$("#logoContainer").append((new Hexagon("projectLogo", stevenSketch, [], "100%")).createElement())
-
-
-let hexagon = {
-    containers: $("#ProjectScreen svg"),
-    pattern: $("#ProjectScreen .hexPattern")[0],
-    element: (Object.values($("#ProjectScreen .hexagon")))[0],
-
-
-    addEventListeners: function() {
-
-
-        this.element.addEventListener("click", () => {
-            soundEffects.playError();
-        })
-
-        this.element.addEventListener("mouseover", () => {
-            soundEffects.playSelect();
-        });
-    },
-
-    changeHexImage: function(imgSrc) {
-        this.pattern.firstElementChild.attributes[2].nodeValue = imgSrc;
-    }
-
-}
 
 
 let overlayMenu = {
-    element: $("#overlayMenuContainer > svg")[0]
+    element: $("#overlayMenuContainer > svg")[0],
+    logo: {
+        hexagon: new Hexagon("projectLogo", stevenSketch, [], "35%", "none", "0 0 262.5 225", "black", 2.5, 0, "40,33", "0.325", false),
+        pattern: null,
+        element: null,
+
+        create: function () {
+            this.element = this.hexagon.createElement();
+
+            console.log("element", this.element);
+            overlayMenu.element.append(this.element);
+            this.pattern = $("#projectLogo")[0];
+            this.addEventListeners();
+
+            overlayMenu.options.create();
+        },
+
+
+
+        addEventListeners: function () {
+            this.element.addEventListener("click", () => {
+                overlayMenu.options.toggle();
+                soundEffects.playDecision();
+            })
+
+            this.element.addEventListener("mouseover", () => {
+                soundEffects.playSelect();
+            });
+        },
+
+        changeHexImage: function (imgSrc) {
+            console.log("logo: ", this);
+            this.pattern.firstElementChild.attributes[2].nodeValue = imgSrc;
+        }
+    },
+
+    options: {
+        hexagons: [
+            new Hexagon("description", stevenSketch, [], "20%", "none", "0 0 262.5 225", "black", 31.625, -16.65, "40,33", "0.325", false),
+            new Hexagon("gallery", stevenSketch, [], "20%", "none", "0 0 262.5 225", "black", 31.625, 16.65, "40,33", "0.325", false),
+            new Hexagon("technologies", stevenSketch, [], "20%", "none", "0 0 262.5 225", "black", 46.05, 0, "40,33", "0.325", false),
+            new Hexagon("links", stevenSketch, [], "20%", "none", "0 0 262.5 225", "black", 60.49, -16.65, "40,33", "0.325", false),
+            new Hexagon("domainsAndSkills", stevenSketch, [], "20%", "none", "0 0 262.5 225", "black", 60.49, 16.65, "40,33", "0.325", false),
+            new Hexagon("team", stevenSketch, [], "20%", "none", "0 0 262.5 225", "black", 74.915, 0, "40,33", "0.325", false)
+        ],
+
+        patterns: [],
+        elements: [],
+
+        isCreated: false,
+
+        create: function () {
+            this.isCreated = true;
+
+            this.hexagons.forEach(hexagon => {
+                let element = hexagon.createElement();
+
+                overlayMenu.element.append(element);
+
+                this.elements.push(element);
+
+                this.patterns.push($(`#${hexagon.patternID}`)[0]);
+
+                element.addEventListener("click", () => {
+                    soundEffects.playDecision();
+                    overlayMenu.options.destroy();
+                })
+
+
+            });
+
+
+        },
+
+        destroy: function () {
+            this.isCreated = false;
+            this.hexagons.forEach(hexagon => {
+                hexagon.destroyElement()
+            });
+        },
+
+        toggle: function () {
+
+            if (!this.isCreated) {
+                this.create();
+            } else {
+                this.destroy();
+            }
+        },
+    },
+    create: function () {
+        this.logo.create();
+    },
 }
 
-console.log(overlayMenu.element);
-let exampleHexagons=[
-    new Hexagon("lol0",stevenSketch, [],  "35%", "none", "0 0 262.5 225", "black", 2.5, 0, "40,33", "0.325", false),
-    new Hexagon("lol1",stevenSketch, [],  "20%", "none", "0 0 262.5 225", "black", 31.625,-16.65 , "40,33", "0.325", false),
-    new Hexagon("lol2",stevenSketch, [],  "20%", "none", "0 0 262.5 225", "black", 31.625,16.65 , "40,33", "0.325", false),
-    new Hexagon("lol3",stevenSketch, [],  "20%", "none", "0 0 262.5 225", "black", 46.05,0 , "40,33", "0.325", false),
-    new Hexagon("lol4",stevenSketch, [],  "20%", "none", "0 0 262.5 225", "black", 60.49,-16.65 , "40,33", "0.325", false),
-    new Hexagon("lol5",stevenSketch, [],  "20%", "none", "0 0 262.5 225", "black", 60.49,16.65 , "40,33", "0.325", false),
-    new Hexagon("lol6",stevenSketch, [],  "20%", "none", "0 0 262.5 225", "black",74.915,0 , "40,33", "0.325", false),
-];
-
-exampleHexagons.forEach(hexagon => {
-    overlayMenu.element.append(hexagon.createElement())
-});
-
-
+overlayMenu.create();
 
 let backArrow = undefined;
 
 export let projectScreen = {
     elem: $("#ProjectScreen"),
-    control: function(project=null) {
+    control: function (project = null) {
 
-        if (project){
-            console.log("project: ", project);
+        if (project) {
 
-            $("#ProjectScreen > .BackgroundImg")[0].src=project.image
-            // hexagon.changeHexImage(project.icon)
-            
-            // $("#projectTitle").text(project.title);
+            $("#ProjectScreen > .BackgroundImg")[0].src = project.image
+
+            overlayMenu.logo.changeHexImage(project.icon)
         }
 
         setTimeout(() => {
